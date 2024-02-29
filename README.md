1a6f032c1c4b13845eb1d658ee7a03407a1274a3
jermaine <jblearn2023@gmail.com>
2024-02-29 23:41:32 +0000

    updates

---
+++ README.md
@@ -0,0 +1,31 @@
+
+<h1 align="center">readme generator 👋</h1>
+
+## Description
+🔍 it generates read me files
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+💾 run npm installer
+
+## Usage
+💻 no
+
+## License
+
+The application uses Apache license. 
+
+## Contributing
+👪 yes
+
+## Tests
+✏️ yes
+  
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,36 @@
-// function to generate markdown for README
+// generateMarkdown function populating the README.md
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+<h1 align="center">${data.projectTitle} 👋</h1>
 
-`;
+## Description
+🔍 ${data.description}
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+💾 ${data.installation}
+
+## Usage
+💻 ${data.usage}
+
+## License
+
+The application uses ${data.license} license. 
+
+## Contributing
+👪 ${data.contributing}
+
+## Tests
+✏️ ${data.tests}
+  `;
 }
 
 module.exports = generateMarkdown;
